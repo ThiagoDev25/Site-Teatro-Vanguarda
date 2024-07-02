36bc9457e504cc12cbee3618ff2537891d8ba648
ThiagoDev25 <thiagoafonsopaz@hotmail.com>
2024-07-02 07:10:51 -0300

    configurada a home.jsx

--- src/frontend/src/routes/Home.jsx
+++ src/frontend/src/routes/Home.jsx
@@ -1,7 +1,8 @@
-import axios from 'axios';
+import blogFetch from "../axios/config";
+
 import {useState, useEffect} from 'react';
 
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
 
 import "./routes/Home.css";
 
@@ -13,27 +14,36 @@ const Home = () => {
 
    try {
 
-  const response = await axios("https://jsonplaceholder.typicode.com/posts");
+  const response = await blogFetch.get('/posts');
 
   const data = response.data;
 
-  console.log(data);
+  setPosts(data);
   } catch (error) {
     console.error(error);
    }
-
   };
   
   useEffect(() => {
 
     getPosts();
 
-  }, [])
-  
-  
+  }, []);
+
   return (
-    <div>home</div>
-  )
-}
+    <div className="home">
+      <h1>Úlltimos Posts</h1>
+      {posts.length === 0 ? (<p>Carregando...</p>) : (
+        posts.map((post) => ( 
+          <div className="post" key={post.id}> <h2>{post.title}</h2>
+          <p>{post.body}</p>
+          <Link className="btn" to={`/posts/${post.id}`}>Ler mais
+          </Link>
+    </div>
+        ))
+      )}
+      </div>
+  );
+};
 
 export default Home
